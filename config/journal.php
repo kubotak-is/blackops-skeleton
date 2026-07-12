45527d6dfac7b076f18b69b98b598f15ab561a10
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-13 01:13:38 +0900

    feat: compose public journal observation

--- config/journal.php
+++ config/journal.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
-return [];
+return [
+    'jsonl' => [
+        'enabled' => true,
+        'path' => dirname(__DIR__) . '/var/log/journal.jsonl',
+        'delivery' => 'best_effort',
+    ],
+];
