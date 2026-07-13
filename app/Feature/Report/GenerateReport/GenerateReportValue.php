f1b090d62e34c38b52b432ade95dd9b007b303f6
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-14 02:24:31 +0900

    feat: add http validation lifecycle

--- app/Feature/Report/GenerateReport/GenerateReportValue.php
+++ app/Feature/Report/GenerateReport/GenerateReportValue.php
@@ -7,14 +7,17 @@ namespace App\Feature\Report\GenerateReport;
 use BlackOps\Core\Attribute\Sensitive;
 use BlackOps\Core\Attribute\SensitiveMode;
 use BlackOps\Core\OperationValue;
+use BlackOps\Core\Validation\Attribute\NotBlank;
 use SensitiveParameter;
 
 final readonly class GenerateReportValue implements OperationValue
 {
     public function __construct(
+        #[NotBlank]
         public string $reportName,
         #[Sensitive(SensitiveMode::Mask)]
         #[SensitiveParameter]
+        #[NotBlank]
         public string $apiToken,
     ) {}
 }
