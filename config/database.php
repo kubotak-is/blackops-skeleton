c5720b60fb181e9e945e1793c76bae0d4b558b61
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-18 04:04:27 +0900

    feat: add named database dependency injection

--- config/database.php
+++ config/database.php
@@ -3,13 +3,19 @@
 declare(strict_types=1);
 
 return [
-    'connection' => [
-        'driver' => 'pdo_pgsql',
-        'host' => $_ENV['POSTGRES_HOST'] ?? '127.0.0.1',
-        'port' => (int) ($_ENV['POSTGRES_PORT'] ?? '5432'),
-        'dbname' => $_ENV['POSTGRES_DB'] ?? 'blackops',
-        'user' => $_ENV['POSTGRES_USER'] ?? 'blackops',
-        'password' => $_ENV['POSTGRES_PASSWORD'] ?? 'blackops',
+    'default' => 'app',
+    'connections' => [
+        'app' => [
+            'driver' => 'pdo_pgsql',
+            'host' => $_ENV['POSTGRES_HOST'] ?? '127.0.0.1',
+            'port' => (int) ($_ENV['POSTGRES_PORT'] ?? '5432'),
+            'dbname' => $_ENV['POSTGRES_DB'] ?? 'blackops',
+            'user' => $_ENV['POSTGRES_USER'] ?? 'blackops',
+            'password' => $_ENV['POSTGRES_PASSWORD'] ?? 'blackops',
+        ],
     ],
-    'schema' => $_ENV['BLACKOPS_SCHEMA'] ?? 'blackops',
+    'framework' => [
+        'connection' => 'app',
+        'schema' => $_ENV['BLACKOPS_SCHEMA'] ?? 'blackops',
+    ],
 ];
