c024beaa08c71332cae44db19768dda0b4bf3d4c
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-19 14:15:49 +0900

    feat: add frontend contract manifest

--- config/app.php
+++ config/app.php
@@ -9,6 +9,7 @@ return [
         'application_build_id' => $_ENV['APP_BUILD_ID'] ?? 'quickstart-local',
         'operation_manifest' => dirname(__DIR__) . '/var/build/operations.php',
         'http_manifest' => dirname(__DIR__) . '/var/build/http.php',
+        'frontend_manifest' => dirname(__DIR__) . '/var/build/frontend.php',
         'container' => dirname(__DIR__) . '/var/build/container.php',
         'container_class' => 'CompiledContainer',
         'container_namespace' => 'App\\Generated',
