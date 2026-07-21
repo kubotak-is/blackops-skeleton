bd823432cdbc74315856b834b605f06c8fd147e1
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-22 05:05:01 +0900

    feat: discover application console commands

--- config/app.php
+++ config/app.php
@@ -11,9 +11,13 @@ return static fn(Environment $env): array => [
         'operation_manifest' => dirname(__DIR__) . '/var/build/operations.php',
         'http_manifest' => dirname(__DIR__) . '/var/build/http.php',
         'frontend_manifest' => dirname(__DIR__) . '/var/build/frontend.php',
+        'command_manifest' => dirname(__DIR__) . '/var/build/commands.php',
         'container' => dirname(__DIR__) . '/var/build/container.php',
         'container_class' => 'CompiledContainer',
         'container_namespace' => 'App\\Generated',
+    ],
+    'command_discovery' => [
+        dirname(__DIR__) . '/app',
     ],
     'services' => [
         ApplicationServiceProvider::class,
