28faae16bf59e870546468652347ca1ee903e49b
kubotak-is <kenjiro.kubota@gmail.com>
2026-07-23 01:28:33 +0900

    Add application environment file bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,24 +3,7 @@
 declare(strict_types=1);
 
 use BlackOps\Application\Application;
-use Dotenv\Dotenv;
 
 $basePath = dirname(__DIR__);
-$processEnvironment = getenv();
 
-foreach ($processEnvironment as $name => $value) {
-    if (is_string($name) && is_string($value) && !array_key_exists($name, $_ENV)) {
-        $_ENV[$name] = $value;
-    }
-}
-
-Dotenv::createImmutable($basePath)->safeLoad();
-
-$environment = [];
-foreach ($_ENV as $name => $value) {
-    if (is_string($name) && is_string($value)) {
-        $environment[$name] = $value;
-    }
-}
-
-return Application::configure($basePath)->withEnvironment($environment)->withConfiguration()->create();
+return Application::configure($basePath)->withEnvironmentFile()->withConfiguration()->create();
